2ad4efec81ac311d908994fd27504c80335064a8
happilin <happilin@gmail.com>
2022-09-16 20:17:14 +0900

    0916 2

---
+++ src/main/resources/static/js/item.js
@@ -0,0 +1,92 @@
+<script>
+        function common() {
+            document.getElementById("bf_item").style.display = "none";
+            document.getElementById("speed_item").style.display = "none";
+            document.getElementById("cn_item").style.display = "none";
+            document.getElementById("nc_item").style.display = "none";
+            document.getElementById("us_item").style.display = "none";
+            document.getElementById("gt_item").style.display = "none";
+            document.getElementById("gb_item").style.display = "none";
+            document.getElementById("pl_item").style.display = "none";
+            document.getElementById("gl_item").style.display = "none";
+
+        }
+        function onClickBfEvent() {
+            if (document.getElementById("bf_item").style.display === "none") {
+                common();
+                document.getElementById("bf_item").style.display = "block";
+            } else {
+                common();
+            }
+        }
+        function onClickSpeedEvent() {
+            if (document.getElementById("speed_item").style.display === "none") {
+                common();
+                document.getElementById("speed_item").style.display = "block";
+            } else {
+                common();
+            }
+        }
+        function onClickcnEvent() {
+            if (document.getElementById("cn_item").style.display === "none") {
+                common();
+                document.getElementById("cn_item").style.display = "block";
+            } else {
+                common();
+            }
+        }
+        function onClickncEvent() {
+            if (document.getElementById("nc_item").style.display === "none") {
+                common();
+                document.getElementById("nc_item").style.display = "block";
+            } else {
+                common();
+            }
+        }
+        function onClickusEvent() {
+          if (document.getElementById("us_item").style.display === "none") {
+                common();
+                document.getElementById("us_item").style.display = "block";
+            } else {
+                common();
+            }
+        }
+        function onClickgtEvent(){
+         if (document.getElementById("gt_item").style.display === "none") {
+                common();
+                document.getElementById("gt_item").style.display = "block";
+            } else {
+                common();
+            }
+        }
+        function onClickgbEvent(){
+         if (document.getElementById("gb_item").style.display === "none") {
+                common();
+                document.getElementById("gb_item").style.display = "block";
+            } else {
+                common();
+            }
+        }
+        function onClickplEvent(){
+         if (document.getElementById("pl_item").style.display === "none") {
+                common();
+                document.getElementById("pl_item").style.display = "block";
+            } else {
+                common();
+            }
+        }
+        function onClickglEvent(){
+         if (document.getElementById("gl_item").style.display === "none") {
+                common();
+                document.getElementById("gl_item").style.display = "block";
+            } else {
+                common();
+            }
+        }
+        
+
+
+
+
+
+    </script>
