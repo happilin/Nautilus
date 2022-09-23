cae3420893f1d55d8280c4679f6087c32e351842
happilin <happil38@gmail.com>
2022-09-23 12:59:42 +0900

    0923

--- src/main/java/com/nautilus/NautilusApplication.java
+++ src/main/java/com/nautilus/NautilusApplication.java
@@ -2,10 +2,17 @@ package com.nautilus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NautilusApplication {
+public class NautilusApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(NautilusApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(NautilusApplication.class, args);
 	}
